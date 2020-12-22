0a8d4634a84e578c805f6a45e276fddb3bc7c85e
SafaParveen <safa2222parveen@gmail.com>
2020-12-22 13:24:09 +0530

    Login screen 02

---
+++ app/src/main/java/com/example/opticiansitwa/login/Act_Login.java
@@ -0,0 +1,16 @@
+package com.example.opticiansitwa.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.opticiansitwa.R;
+
+public class Act_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act__login);
+    }
+}
