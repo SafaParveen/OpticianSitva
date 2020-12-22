2ce966f815575cd8e796055354e6fea7083bde43
jonathanab_4 <jonathanidiculla4@gmail.com>
2020-12-22 12:00:18 +0530

    basic_ui_2

--- app/src/main/java/com/example/opticiansitwa/intro/Act_Intro.java
+++ app/src/main/java/com/example/opticiansitwa/intro/Act_Intro.java
@@ -1,14 +1,16 @@
-package com.example.opticiansitwa;
+package com.example.opticiansitwa.intro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.opticiansitwa.R;
+
+public class Act_Intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.act_intro);
     }
 }
