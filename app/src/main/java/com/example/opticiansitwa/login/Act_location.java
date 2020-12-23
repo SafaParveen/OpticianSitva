81f06bdd0cde4cc3d340ff1aa60fbdcef91867a6
SafaParveen <safa2222parveen@gmail.com>
2020-12-23 13:12:26 +0530

    Authentication 01

--- app/src/main/java/com/example/opticiansitwa/login/Act_location.java
+++ app/src/main/java/com/example/opticiansitwa/login/Act_location.java
@@ -1,6 +1,5 @@
-package com.example.opticiansitwa;
+package com.example.opticiansitwa.login;
 
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -16,10 +15,10 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
+import com.example.opticiansitwa.R;
 import com.example.opticiansitwa.databinding.ActLocationBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
